dd7484084c5e3b5bb0c9c5dd02b2cdadd67b8338
leticiafarias <leticia_farias@alu.ufc.br>
2021-10-06 12:40:38 -0300

    Add basic challeges

---
+++ Easy/2-inputAndOutput.cpp
@@ -0,0 +1,20 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+
+  int a, b, c;
+
+  cin >> a >> b >> c;
+
+  int sum = a + b + c;
+
+  std::cout << sum << endl;
+
+  return 0;
+}
